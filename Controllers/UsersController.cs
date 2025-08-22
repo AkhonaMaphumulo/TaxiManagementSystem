e4599991a21e95e6bad5d105b707a6919cecc5ca
AkhonaMaphumulo <akhonamaphumulo24@gmail.com>
2025-08-22 14:55:10 +0200

    i have added all the files that fall under my code

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,119 @@
+﻿using SignUpCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignUpCode.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult PassengerOperationPage()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+
+        private registrationDB db = new registrationDB();
+
+        public ActionResult PassengerSignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+
+
+        public ActionResult PassengerSignUp(User obj)
+        {
+            try
+            {
+                if (db.Users.Any(x => x.Email == obj.Email))
+                {
+                    ViewBag.Notification = "An account with the same Email(Username) already exists";
+                    return View();
+                }
+                else if (ModelState.IsValid)
+                {
+                    db.Users.Add(obj);
+                    db.SaveChanges();
+
+                    // Set session variables
+                    Session["Email"] = obj.Email;
+                    Session["Passenger_Name"] = obj.Passenger_Name;
+
+                    // Redirect to SignIn Page
+                    return RedirectToAction("PassengerSignIn");
+                }
+
+            }
+            catch (DbEntityValidationException ex)
+            {
+                // Log or inspect the validation errors
+                var errorMessages = ex.EntityValidationErrors
+                    .SelectMany(e => e.ValidationErrors)
+                    .Select(e => e.ErrorMessage)
+                    .ToList();
+
+                // You can log these errors or display them to the user
+                ViewBag.Notifica = string.Join(", ", "Enter all fields");
+                return View();
+            }
+            return View();
+
+        }
+
+
+        //CUSTOMER SIGN IN PAGE
+
+        [HttpGet]
+        public ActionResult PassengerSignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult PassengerSignIn(User logain)
+        {
+           
+
+            using (registrationDB db = new registrationDB())
+            {
+                var Password = db.Users.Where(x => x.Password.Equals(logain.Password)).FirstOrDefault();
+                var Username = db.Users.Where(x => x.Email.Equals(logain.Email)).FirstOrDefault();
+                var both = db.Users.Where(x => x.Email.Equals(logain.Email) && x.Password.Equals(logain.Password)).FirstOrDefault();
+
+                if (both != null)
+                {
+                    Session["int"] = logain.Email.ToString();
+                    Session["Email"] = logain.Email.ToString();
+                    return RedirectToAction("PassengerOperationPage");
+                }
+                else if (Password == null)
+                {
+                    ViewBag.NotifyPass = "Wrong Password";
+                }
+                else if (Username == null)
+                {
+                    ViewBag.NotifyUserN = "Wrong Username";
+                }
+                else
+                {
+                    ViewBag.Notify = "n";
+
+                }
+            }
+            return View();
+
+        }
+
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SignUpCode.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters.")]
+        [Column("Passenger_Name")]
+        public string Passenger_Name { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        [StringLength(255)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match. Please re-enter.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [DataType(DataType.Password)]
+        [StringLength(255)]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+
+    public class registrationDB : DbContext
+    {
+        public registrationDB() : base("TMUsersLogin")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+
+
+
+}
+
